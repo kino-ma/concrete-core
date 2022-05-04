ed87c439454419c2ab0fcfd93202a45b2afbcfce
Ben <ben.curtis@zama.ai>
2022-05-04 11:01:09 +0100

    update spec

--- concrete-core/src/specification/engines/glwe_secret_key_tensor_product.rs
+++ concrete-core/src/specification/engines/glwe_secret_key_tensor_product.rs
@@ -1,14 +1,15 @@
 use super::engine_error;
 use crate::specification::engines::AbstractEngine;
 use crate::specification::entities::GlweSecretKeyEntity;
+use std::process::Output;
 
 engine_error! {
-    GlweKeyTensorProductError for GlweKeyTensorProductEngine @
+    TensorProductGlweSecretKeyCreationError for TensorProductGlweSecretKeyCreationEngine @
     PolynomialSizeMismatch => "The polynomial size of the two input keys is not the same",
     InputGlweDimensionMismatch => "The Glwe dimension of the two input keys is not the same"
 }
 
-impl<EngineError: std::error::Error> GlweKeyTensorProductError<EngineError> {
+impl<EngineError: std::error::Error> TensorProductGlweSecretKeyCreationError<EngineError> {
     pub fn perform_generic_checks<InputKey1, InputKey2>(
         input1: &InputKey1,
         input2: &InputKey2,
@@ -35,27 +36,29 @@ impl<EngineError: std::error::Error> GlweKeyTensorProductError<EngineError> {
 ///
 /// The goal of this function is to take as input two GLWE secret keys s1 and s2, and
 /// create their tensor product s1 x s2
-pub trait GlweKeyTensorProductEngine<InputKey1, InputKey2>: AbstractEngine
+pub trait TensorProductGlweSecretKeyCreationEngine<InputKey1, InputKey2, OutputKey>: AbstractEngine
 where
     InputKey1: GlweSecretKeyEntity,
     InputKey2: GlweSecretKeyEntity<KeyDistribution = InputKey1::KeyDistribution>,
+    // Do we want to include a trait bound on the output key?
+    OutputKey: GlweSecretKeyEntity,
 {
-    fn tensor_product_glwe_secret_key(
+    fn create_tensor_product_glwe_secret_key(
         &mut self,
         input1: InputKey1,
         input2: InputKey2,
-    ) -> Result<(), GlweKeyTensorProductError<Self::EngineError>>;
+    ) -> Result<OutputKey, TensorProductGlweSecretKeyCreationError<Self::EngineError>>;
 
     /// Unsafely performs a tensor product of two GLWE secret keys.
     ///
     /// # Safety
     /// For the _general_ safety concerns regarding this operation, refer to the different variants
-    /// of [`GlweKeyTensorProductError`]. For safety concerns _specific_ to an engine, refer to
-    /// the implementer safety section.
+    /// of [`TensorProductGlweSecretKeyCreationError`]. For safety concerns _specific_ to an engine,
+    /// refer to the implementer safety section.
 
-    unsafe fn tensor_product_glwe_secret_key_unchecked(
+    unsafe fn create_tensor_product_glwe_secret_key_unchecked(
         &mut self,
         input1: InputKey1,
         input2: InputKey2,
-    );
+    ) -> OutputKey;
 }
